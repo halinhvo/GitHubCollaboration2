63eb262997147236af52a209297485bdbbd87719
Miley <cutegoldendragon@gmail.com>
2025-07-28 10:35:55 -0400

    mileys change 4

--- GitHubCollaboration2/ContentView.swift
+++ GitHubCollaboration2/ContentView.swift
@@ -17,6 +17,7 @@ struct ContentView: View {
             Text("Change 1")
             Text("Change 2 (katelynn)")
             Text("Change 3")
+            Text("Change 4 (miley)")
         }
         .padding()
     }

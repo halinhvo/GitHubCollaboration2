d9711906c4601737cf25abe8a4ccd7919476ae3f
Haley Sheth <haley.sheth@gmail.com>
2025-07-28 10:33:42 -0400

    change 3

--- GitHubCollaboration2/ContentView.swift
+++ GitHubCollaboration2/ContentView.swift
@@ -16,6 +16,7 @@ struct ContentView: View {
             Text("Hello, world!")
             Text("Change 1")
             Text("Change 2 (katelynn)")
+            Text("Change 3")
         }
         .padding()
     }
